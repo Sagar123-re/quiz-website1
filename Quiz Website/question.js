91de727170dcf76562448f2220eac8d517a86c6f
Sagar123-re <sagarnautiyal111@gmail.com>
2024-07-19 13:25:31 +0530

    Add files via upload

---
+++ Quiz Website/question.js
@@ -0,0 +1,63 @@
+let questions = [
+    {
+        numb: 1,
+        question:"How many bytes does an int store:",
+        answer: "C. 4 Bytes",
+        options: [
+            "A. 2 Bytes",
+            "B. 8 Bytes",
+            "C. 4 Bytes",
+            "D. 1 Bytes"
+        ]
+},
+    {
+        numb: 2,
+        question: "How is an array initialized in C language?",
+        answer: "A. int a[3]={1,2,3};",
+        options: [
+            "A. int a[3]={1,2,3};",
+            "B. int a ={1,2,3};",
+            "C. int a[]=new int[3]",
+            "D. int a(3)=[1,2,3];"
+
+        ]
+},
+    {
+        numb: 3,
+        question: "Which of the following is not the type of queue?",
+        answer: "B. Single-ended queue",
+        options: [
+            "A. Priority queue",
+            "B. Single-ended queue",
+            "C. Circular queue",
+            "D. Ordinary queue"
+
+        ]
+},
+    {
+        numb: 4,
+        question: "How are String represented in memory in c++",
+        answer: "D. An array of characters.",
+        options: [
+            "A. The object of some class.",
+            "B. Same as other primitive data types.",
+            "C. LinkedList of characters.",
+            "D. An array of characters.",
+
+        ]
+},
+    {
+        numb: 5,
+        question: "Which of the following data structures finds its use in recursion?",
+        answer: "C. Stack",
+        options: [
+            "A. Arrays",
+            "B. LinkedList",
+            "C. Stack",
+            "D. Queues"
+
+        ]
+}
+];
+
+
